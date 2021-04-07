870da57d3ffe18270b65aeaa20ae6aa675db5ee6
tskluzac <skluzacek@uchicago.edu>
2021-04-07 15:22:01 -0500

    Pushing the Midway node version.

--- xtract_tika_main.py
+++ xtract_tika_main.py
@@ -1,35 +1,106 @@
 
-
+import os
 import tika
 import time
 from tika import parser
+from queue import Queue
+import threading
+import json
+from random import randint
+
+class TikaParser:
+
+    def __init__(self, write_path):
+        self.shared_dir_q = Queue()
+        self.mdata_to_save = Queue()
+        self.weird_fails = 0
+        self.runtime_fails = 0
+        self.write_path = write_path
+        self.cur_val = 0
+
+    def thr_extract_metadata(self):
+
+        while not self.shared_dir_q.empty():
 
 
-def extract_tika(filename):
-    t_start = time.time()
-    tika.TikaClientOnly = True
+            item = self.shared_dir_q.get()
 
-    # file_to_proc = "/Users/tylerskluzacek/Desktop/conference-proceeding.pdf"
-    # file_to_proc = "/Users/tylerskluzacek/Desktop/king-cholera.jpeg"
+            try:
+                # opening pdf file
+                time.sleep(0.01)  # minimum amount of buffer time to keep Tika from crashing with 'restart' issues.
+                                  # Note that this is faster than simply 'not' sleeping (or sleeping < 10ms).
 
-    # opening pdf file
-    parsed_pdf = parser.from_file(filename)
+                t_ext_start = time.time()
+                parsed_pdf = parser.from_file(item)
+            except IsADirectoryError as e:
+                print("Actually a directory. Skipping!")
+                continue
+            except OSError as e2:
+                print(e2)
+                self.weird_fails += 1
+                time.sleep(2)
+                print(f"Current weird fails: {self.weird_fails}")
+                continue
+            except RuntimeError as e:
+                self.runtime_fails += 1
+                print(f"Current runtime fails: {self.runtime_fails}")
+                continue
 
-    # saving content of pdf
-    # you can also bring text only, by parsed_pdf['text']
-    # parsed_pdf['content'] returns string
-    data = parsed_pdf['metadata']
+            # try:
+            # opening
 
-    # Printing of content
-    # print(data)
-    t_end = time.time()
-    # print(f"Total time: {t_end - t_start}")
+            # saving content of pdf
+            # you can also bring text only, by parsed_pdf['text']
+            # parsed_pdf['content'] returns string
+            data = parsed_pdf['metadata']
+            self.mdata_to_save.put('hi')
 
-    # <class 'str'>
-    # print(type(data))
-    return {'mdata': data, 'tot_time': t_end-t_start}
+            write_path = os.path.join(self.write_path, str(self.cur_val))
+            self.cur_val += 1
+            t_ext_end = time.time()
+            with open(write_path, 'w') as f:
+                json.dump({'ext_time': t_ext_end-t_ext_start, 'mdata': data, 'timestamp': time.time()}, f)
+
+    def extract_tika(self, dir_name, pct_to_transfer=0, num_thr=1):
+        t0 = time.time()
+        print("Phase 1: Configuring Tika...")
+        tika.TikaClientOnly = True
+
+        print("Phase 2: Crawling the files. Creating shared file list...")
+        # First create full list of files in directory
+        all_subdirs = os.listdir(dir_name)
+
+        for subdir in all_subdirs:
+            dir_full_path = os.path.join(dir_name, subdir)
+
+            if os.path.isdir(dir_full_path):
+                all_files = os.listdir(dir_full_path)
+
+                for filename in all_files:
+                    file_full_path = os.path.join(dir_full_path, filename)
+                    self.shared_dir_q.put(file_full_path)
+            # Otherwise it's a file.
+            else:
+                self.shared_dir_q.put(dir_full_path)
+
+        print(f"Phase 3: Files crawled! Spinning up {num_thr} processing threads...")
+        # Spinning up n threads -- this should be configured to the number of available cores.
+        for i in range(0, num_thr):
+            thr = threading.Thread(target=self.thr_extract_metadata, args=())
+            thr.start()
+
+        print(f"Number of files: {self.shared_dir_q.qsize()}")
+        while True:
+            print(f"Time elapsed: {time.time() - t0}")
+            print(f"Number files extracted: {self.mdata_to_save.qsize()}")
+            time.sleep(1)
 
 
 if __name__ == '__main__':
-    file_to_proc = "/king-cholera.jpeg"
-    print(extract_tika(file_to_proc))
+    # TODO: move a bunch of these to the commandline.
+    dir_to_proc = "/project2/chard/skluzacek/data_to_process"
+    # dir_to_proc = '/Users/tylerskluzacek/Desktop'
+    # write_dir = '/Users/tylerskluzacek/Desktop/mdata'
+    write_dir = '/project2/chard/skluzacek/mdata'
+    tp = TikaParser(write_path=write_dir)
+    tp.extract_tika(dir_to_proc, pct_to_transfer=0, num_thr=27)
